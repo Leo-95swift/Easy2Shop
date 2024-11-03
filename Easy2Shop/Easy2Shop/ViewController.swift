ce187b298f9a6d62636b1352b7d4775febadc9a3
xach55 <xach55@gmail.com>
2024-11-03 15:19:19 +0400

    initial commit

---
+++ Easy2Shop/Easy2Shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy2Shop
+//
+//  Created by Levon Shaxbazyan on 03.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
